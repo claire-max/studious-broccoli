9e80847f666af5b58efb2e5ec13d237f07707a2b
ClaireFree <claire@robingoodfoods.com>
2022-10-10 17:43:10 -0700

    added license info

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,64 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+function userInput(){
+    return inquirer.prompt([
+    {name: 'license',
+    message: 'Select kind of license for this application:',
+    type: 'list',
+    choices: [
+    "Apache license 2.0", 
+    "Boost Software License 1.0", 
+    "ISC",  
+    "MIT", 
+    "Mozilla Public License 2.0",  
+    "The Unlicense", 
+    "zLib License"
+]},
+    {name: 'title',
+    message: 'What is the title of this application?',
+    type: 'input'},
+    {name: 'description',
+    message: 'What does this application do?',
+    type: 'input'},
+    {name: 'install',
+    message: 'How do I install this application?',
+    type: 'input'},
+    {name: 'usage',
+    message: 'How do I use this application?',
+    type: 'input'},
+    {name: 'contributing',
+    message: 'How do I contribute to this application?',
+    type: 'input'},
+    {name: 'testing',
+    message: 'How do I test this application?',
+    type: 'input'},
+    {name: 'username',
+    message: 'What is your Github username?',
+    type: 'input'},
+    {name: 'email',
+    message: 'What is your e-mail?',
+    type: 'input'},
+    {name: 'fileName',
+    message: 'What do you want to call this readme?',
+    type: 'input'},
+])};
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.appendFile(`${fileName}.md`, data,
+    (err) => err ? console.error(err) : console.log(`${fileName}.md has been generated. `))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    let answers = await userInput();
+    writeToFile((answers.fileName),(generateMarkdown(answers)));
+}
 
 // Function call to initialize app
 init();
